12c4dd34e165118b3dcb81391df10bde07c8d3f1
Athesdrake <athesdrake@gmail.com>
2019-01-13 20:53:51 +0100

    Typo

--- fromage/errors.py
+++ fromage/errors.py
@@ -4,59 +4,59 @@ class InvalidVersion(Exception):
 		message = 'The installed version of {} is not supported. Use the version {} instead.'
 		super().__init__(message.format(module, version))
 
-class FromaigeError(Exception):
-	"""Base exception class for Fromaige."""
+class FromageError(Exception):
+	"""Base exception class for Fromage."""
 	def __init__(self, **params):
 		message = self.__doc__.format(**params)
 		super().__init__(message)
 
-class AlreadyConnected(FromaigeError):
+class AlreadyConnected(FromageError):
 	"""This instance is already connected, disconnect first."""
-class EnumOutOfRange(FromaigeError):
+class EnumOutOfRange(FromageError):
 	"""Enum value out of range."""
-class ImageIdError(FromaigeError):
+class ImageIdError(FromageError):
 	"""An image id can not be a number."""
-class InternalError(FromaigeError):
-	"""Internal error."""
-class InvalidDate(FromaigeError):
+class InternalError(FromageError):
+	"""Internal error. (0x{:x})"""
+class InvalidDate(FromageError):
 	"""Invalid date format. Expected: dd/mm/yyyy"""
-class InvalidEnum(FromaigeError):
+class InvalidEnum(FromageError):
 	"""Invalid enum."""
-class InvalidExtension(FromaigeError):
+class InvalidExtension(FromageError):
 	"""Provided file url or name does not have a valid extension."""
-class InvalidFile(FromaigeError):
+class InvalidFile(FromageError):
 	"""Provided file does not exist."""
-class InvalidForumUrl(FromaigeError):
+class InvalidForumUrl(FromageError):
 	"""Invalid Atelier801's url. ({})"""
-class InvalidId(FromaigeError):
+class InvalidId(FromageError):
 	"""Invalid id."""
-class InvalidUser(FromaigeError):
+class InvalidUser(FromageError):
 	"""The user does not exist or was not found."""
-class NoPollResponses(FromaigeError):
+class NoPollResponses(FromageError):
 	"""Missing poll responses. There must be at least two responses."""
-class NoRequiredFields(FromaigeError):
+class NoRequiredFields(FromageError):
 	"""The fields {} are needed."""
-class NoTribeError(FromaigeError):
+class NoTribeError(FromageError):
 	"""This instance does not have a tribe."""
-class NoUrlLocation(FromaigeError):
+class NoUrlLocation(FromageError):
 	"""Missing location."""
-class NoUrlLocationPrivate(FromaigeError):
+class NoUrlLocationPrivate(FromageError):
 	"""The fields {} are needed if the object is private."""
-class NotConnected(FromaigeError):
+class NotConnected(FromageError):
 	"""This instance is not connected yet, connect first."""
-class NotPollError(FromaigeError):
+class NotPollError(FromageError):
 	"""Invalid topic. Poll not detected."""
-class NotVerifiedError(FromaigeError):
+class NotVerifiedError(FromageError):
 	"""This instance has not a certificate yet. Valid the account first."""
-class PollIdError(FromaigeError):
+class PollIdError(FromageError):
 	"""A poll id can not be a string."""
-class PollOptionNotFound(FromaigeError):
+class PollOptionNotFound(FromageError):
 	"""Invalid poll option."""
-class SecretKeyNotFound(FromaigeError):
+class SecretKeyNotFound(FromageError):
 	"""Secret keys could not be found."""
-class UnaivalableEnum(FromaigeError):
+class UnaivalableEnum(FromageError):
 	"""This function does not accept this enum."""
-class Unauthorized(FromaigeError):
+class Unauthorized(FromageError):
 	"""You don't have rights to see this info."""
 
 # Create all errors based on ErrorString Enum
@@ -66,8 +66,8 @@ class Unauthorized(FromaigeError):
 # 		name = ''.join([n.capitalize() for n in name.split('_')]) + 'Error'
 
 # 		def init(self, *args):
-# 			super(FromaigeError, self).__init__(self.__doc__.format(*args))
-# 		error = type(name, (FromaigeError,), dict(__init__=init))
+# 			super(FromageError, self).__init__(self.__doc__.format(*args))
+# 		error = type(name, (FromageError,), dict(__init__=init))
 # 		error.__doc__ = msg
 
 # 		globals()[name] = error
